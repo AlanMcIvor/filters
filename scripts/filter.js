a1a8bc1788b2efd48c5294c6e976e1ac6222576a
alan mcivor <alan.mcivor27@gmail.com>
2023-09-20 09:27:45 +0100

    added search input box

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     
-    // imitial filter - shows all images
-    $('.image-container div').show();
+    // // imitial filter - shows all images
+    // $('.image-container div').show();
 
-    // click event handler for filter buttons
-    $('.filter-buttons button').click(function(){
-        const filter = $(this).data('filter');
+    // // click event handler for filter buttons
+    // $('.filter-buttons button').click(function(){
+    //     const filter = $(this).data('filter');
 
-        // show all images when all is clicked
-        if(filter === 'all'){
-            $('.image-container div').show();
-        } else {
-            // hide all images
-            $('.image-container div').hide();
-            // show all images with the selected filter class
-            $(`.image-${filter}`).show();
-        }
-    })
+    //     // show all images when all is clicked
+    //     if(filter === 'all'){
+    //         $('.image-container div').show();
+    //     } else {
+    //         // hide all images
+    //         $('.image-container div').hide();
+    //         // show all images with the selected filter class
+    //         $(`.image-${filter}`).show();
+    //     }
+    // })
 
 
 })
